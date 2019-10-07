74af048bbfc3de44131626e564feb23b764bb097
Hoxtygen <hoxtygen@live.com>
2019-10-07 15:55:59 +0100

    Refactor selection sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,18 +1,14 @@
 # TO-DO: Complete the selection_sort() function below
-def swap(arr, x, y):
-    temp = arr[x]
-    arr[x] = arr[y]
-    arr[y] = temp
-    return arr
-
-def selection_sort( arr ):
-    for i in range(0, len(arr)):
-        cur_index = i
-        smallest_index = cur_index
-        for j in range(i+1, len(arr)):
-          if(arr[i] > arr[j]):
-            swap(arr, cur_index, j)
-    return arr
+def selection_sort(arr):
+  arrLen = len(arr)
+  for i in range(arrLen -1):
+    cur_index = i
+    smallest_index = cur_index
+    for j in range(cur_index, arrLen):
+      if arr[j] < arr[smallest_index]:
+        smallest_index = j
+    arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
+  return arr
 
 print(selection_sort([5, 3, 6, 2, 10]))
 
