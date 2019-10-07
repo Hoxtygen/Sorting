c11a28e00bf50d77f5609a49b32c0233f98b36fa
Hoxtygen <hoxtygen@live.com>
2019-10-08 00:04:55 +0100

    Solve linear search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,17 +1,21 @@
-# STRETCH: implement Linear Search				
+# STRETCH: implement Linear Search
 def linear_search(arr, target):
-  
+  arrLen = len(arr)
   # TO-DO: add missing code
+  for i in range(arrLen):
+      if target == arr[i]:
+          return 1;
 
-   return -1   # not found
+  return -1   # not found
 
+print(linear_search([2, 4, 6, 9, 5], 100))
 
-# STRETCH: write an iterative implementation of Binary Search 
+# STRETCH: write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
   if len(arr) == 0:
     return -1 # array empty
-    
+
   low = 0
   high = len(arr)-1
 
@@ -20,9 +24,9 @@ def binary_search(arr, target):
   return -1 # not found
 
 
-# STRETCH: write a recursive implementation of Binary Search 
+# STRETCH: write a recursive implementation of Binary Search
 def binary_search_recursive(arr, target, low, high):
-  
+
   middle = (low+high)//2
 
   if len(arr) == 0:
